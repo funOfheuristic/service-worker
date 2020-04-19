841795158a038a996bbdea3d2641c8db5d3262a2
Subrat <mastersubrat007@gmail.com>
2020-04-19 23:20:31 +0530

    Service Worker basic

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+const cacheArr = ['/'];
+const CACHE_NAME = 'cache-v10';
+self.addEventListener('install', (event) => {
+    console.log('worker is installed');
+    // event.waitUntil(
+    //     caches.open(CACHE_NAME).then((cache) => {
+    //       console.log("Opened cache");
+    //       cache.addAll(cacheArr).then(() => self.skipWaiting());
+    //     })
+    // );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (CACHE_NAME !== cacheName) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
+
+//   self.addEventListener('fetch', (event) => {
+//     event.respondWith(
+//       caches.match(event.request)
+//         .then((response) => {
+//           // Cache hit - return response
+//           if (response) {
+//             return response;
+//           }
+//           return fetch(event.request).catch(() => () => caches.match(event.request));
+//         }
+//       )
+//     );
+//   });
+
+self.addEventListener("fetch", (fetchEvent) => {
+    fetchEvent.respondWith(
+        fetch(fetchEvent.request).then(res => {
+            const cacheRes = res.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => cache.put(fetchEvent.request, cacheRes));
+  ß
+            return res;
+        }).catch(() => caches.match(fetchEvent.request).then(res => res))
+    );
+  });
+
+
